84bf5b865755677303f83da64e6d7d4d665148e1
Sagrika Trehan <trehansagrika0897@gmail.com>
2026-01-14 21:02:51 +0530

    today updated

--- controllers/listings.js
+++ controllers/listings.js
@@ -42,6 +42,50 @@ router.get('/:listingId', async (req, res) => {
   }
 });
 
+router.delete('/:listingId', async (req, res) => {
+  try {
+    const listing = await Listing.findById(req.params.listingId);
+    if (listing.owner.equals(req.session.user._id)) {
+      await listing.deleteOne();
+      res.redirect('/listings');
+    } else {
+      res.send("You don't have permission to do that.");
+    }
+  } catch (error) {
+    console.error(error);
+    res.redirect('/');
+  }
+});
+
+
+router.get('/:listingId/edit', async (req, res) => {
+  try {
+    const currentListing = await Listing.findById(req.params.listingId);
+    res.render('listings/edit.ejs', {
+      listing: currentListing,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
+router.put('/:listingId', async (req, res) => {
+  try {
+    const currentListing = await Listing.findById(req.params.listingId);
+
+    if (currentListing.owner.equals(req.session.user._id)) {
+      await currentListing.updateOne(req.body);
+      res.redirect('/listings');
+    } else {
+       res.send("You don't have permission to do that.");
+    }
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 // Create a route for POST requests to /listings.
 // console.log() the data being sent from the form.
 // Using res.redirect() redirect to /listings.
